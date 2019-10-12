588114e7a3bde6dfac1fce659a78488b4fd59563
Zack Campbell <zajrik@gmail.com>
2019-10-11 19:54:02 -0500

    Update build scripts and tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,23 @@
+/* eslint-disable @typescript-eslint/typedef */
 const gulp = require('gulp');
-const gulp_ts = require('gulp-typescript');
-const gulp_sourcemaps = require('gulp-sourcemaps');
+const typescript = require('gulp-typescript');
+const eslint = require('gulp-eslint');
+const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
 
-const project = gulp_ts.createProject('tsconfig.json');
+const project = typescript.createProject('tsconfig.json');
 
-let _linter;
-let _gulp_tslint;
-let _tslint;
+gulp.task('lint', () =>
+	gulp.src('src/**/*.ts')
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError()));
 
-const gulp_tslint = () => _gulp_tslint = _gulp_tslint || require('gulp-tslint');
-const tslint = () => _tslint = _tslint || require('tslint');
-const linter = () => _linter = _linter || tslint().Linter.createProgram('tsconfig.json');
-
-gulp.task('lint', () => {
-	return gulp.src('src/**/*.ts')
-		.pipe(gulp_tslint()({
-			configuration: 'tslint.json',
-			formatter: 'prose',
-			program: linter()
-		}))
-		.pipe(gulp_tslint().report());
-})
-
-gulp.task('build', () => {
+gulp.task('build', () =>
+{
 	del.sync(['bin/**/*.*']);
 	const tsCompile = gulp.src('src/**/*.ts')
-		.pipe(gulp_sourcemaps.init({ base: 'src' }))
+		.pipe(sourcemaps.init({ base: 'src' }))
 		.pipe(project());
 
 	tsCompile.pipe(gulp.dest('bin/'));
@@ -36,9 +27,8 @@ gulp.task('build', () => {
 	gulp.src('src/**/*.lang').pipe(gulp.dest('bin/'));
 
 	return tsCompile.js
-		.pipe(gulp_sourcemaps.write('.', { sourceRoot: '../src' }))
+		.pipe(sourcemaps.write('.', { sourceRoot: '../src' }))
 		.pipe(gulp.dest('bin/'));
 });
 
-gulp.task('default', gulp.series('build'));
-gulp.task('build:vscode', gulp.series('lint', 'build'));
+gulp.task('default', gulp.series('lint', 'build'));
