46ce0da1836429367781a29ad3c3c44defae637d
Zack Campbell <zajrik@gmail.com>
2019-10-14 14:38:37 -0500

    Rename tests folder, add StringReader unit tests

---
+++ src/__test__/parser/StringReader.test.ts
@@ -0,0 +1,77 @@
+import { StringReader } from '../../parser/StringReader';
+
+describe('Reading strings with StringReader', () =>
+{
+	it('Should advance by a given amount (consume, discard)', () =>
+	{
+		const reader: StringReader = new StringReader('foobarbaz');
+		expect(reader.column).toBe(1);
+
+		reader.consume();
+		expect(reader.column).toBe(2);
+		reader.consume(2);
+		expect(reader.column).toBe(4);
+
+		reader.discard();
+		expect(reader.column).toBe(5);
+		reader.discard(2);
+		expect(reader.column).toBe(7);
+	});
+
+	it('Should properly handle newlines (consume, discard)', () =>
+	{
+		let reader: StringReader = new StringReader('foo\nbar\nbaz');
+		reader.discard(4);
+		expect(reader.line).toBe(2);
+		expect(reader.column).toBe(1);
+
+		reader = new StringReader('foo\nbar\nbaz');
+		reader.consume(4);
+		expect(reader.line).toBe(2);
+		expect(reader.column).toBe(1);
+
+		reader = new StringReader('foo\nbar\nbaz');
+		reader.discard(10);
+		expect(reader.line).toBe(3);
+		expect(reader.column).toBe(3);
+
+		reader = new StringReader('foo\nbar\nbaz');
+		reader.consume(10);
+		expect(reader.line).toBe(3);
+		expect(reader.column).toBe(3);
+	});
+
+	it('Should peek ahead by a given amount', () =>
+	{
+		const reader: StringReader = new StringReader('foo\nbar\nbaz');
+		expect(reader.peek()).toBe('f');
+		expect(reader.peek(5)).toBe('a');
+	});
+
+	it('Should peek a segment of given length', () =>
+	{
+		const reader: StringReader = new StringReader('foo\nbar\nbaz');
+		expect(reader.peekSegment(5)).toBe('foo\nb');
+	});
+
+	it('Should peek a segment with an offset', () =>
+	{
+		const reader: StringReader = new StringReader('foo\nbar\nbaz');
+		expect(reader.peekSegment(5, 3)).toBe('\nbar\n');
+	});
+
+	it('Should peek behind by a given amount', () =>
+	{
+		const reader: StringReader = new StringReader('foo\nbar\nbaz');
+		reader.discard(10);
+		expect(reader.peekBehind()).toBe('a');
+		expect(reader.peekBehind(6)).toBe('b');
+	});
+
+	it('Should identify EOF', () =>
+	{
+		const reader: StringReader = new StringReader('foo\nbar\nbaz');
+		reader.discard(11);
+		expect(reader.eof()).toBe(true);
+	});
+});
