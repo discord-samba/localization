65456767b65bca0bb1ef9f30848b4ff5708872d2
Zack Campbell <zajrik@gmail.com>
2019-11-15 16:44:24 -0600

    tooling(docs): update typedoc runner

--- scripts/typedoc-runner.js
+++ scripts/typedoc-runner.js
@@ -23,11 +23,18 @@ if (typeof project !== 'undefined')
 		[
 			{
 				label: 'Command',
-				url: 'https://discord-sambo.github.io/command/docs'
+				url: 'https://discord-sambo.github.io/command/docs',
+				current: false
+			},
+			{
+				label: 'Localization',
+				url: 'https://discord-sambo.github.io/localization/docs',
+				current: true
 			},
 			{
 				label: 'Logger',
-				url: 'https://discord-sambo.github.io/logger/docs'
+				url: 'https://discord-sambo.github.io/logger/docs',
+				current: false
 			}
 		]
 	);
