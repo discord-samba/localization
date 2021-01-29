eaf89e7acb058043f2caef06766fd46484dfa409
zajrik <zajrik@users.noreply.github.com>
2021-01-29 18:42:45 +0000

    Push staging from: 2cb20aff6c06a7c71ee03c75cfd308ca63017ede

--- types/LocalizationResourceProxy.ts
+++ types/LocalizationResourceProxy.ts
@@ -1,14 +1,25 @@
-import { TemplateArguments } from '#type/TemplateArguments';
-
 /**
  * Represents a Proxy object for a specific language where function calls
  * will call [Localization.resource()]{@link Localization.resource} for that
  * language and forward the given `TemplateArguments` to the resource lookup.
  *
- * >**TypeScript note**: Can be given a type parameter object where the keys
- * are Localization string keys to provide type hinting on key names when
- * using the proxy
+ * **TypeScript note:** Can be given a type parameter object where the keys
+ * are Localization string keys mapped to type objects that map template keys
+ * to template value types. This will provide type hinting for both template
+ * key names (methods on the proxy) and the template arguments they expect.
+ * Example:
+ *
+ * ```ts
+ * type Resources = { foo: { bar: string, baz?: number } };
+ * let foo: LocalizationResourceProxy<Resources>.foo({ bar: 'baz' });
+ * // The compiler knows that the value of `bar` should be a string, and
+ * // that `baz` expects a number but is optional
+ * ```
  */
-export type LocalizationResourceProxy<T = {}> = {
-	[key in keyof T]: (args?: TemplateArguments) => string
+export type LocalizationResourceProxy<T extends Record<string, Record<string, any>> = {}> = {
+	[K in keyof T]: T[K] extends Record<string, never>
+		? (args?: T[K] & Record<string, any>) => string
+		: (args: T[K] & Record<string, any>) => string;
+} & {
+	[K: string]: (args?: Record<string, any>) => string;
 };
