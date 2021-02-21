7099a66891af80657c8a8f340232b0e5b28968ea
zajrik <zajrik@users.noreply.github.com>
2021-02-21 13:44:50 -0600

    tooling(docs): update typedoc runner, update exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export { Localization } from '#root/Localization';
 export { LocalizationPipeFunction } from '#type/LocalizationPipeFunction';
+export { LocalizationResourceProxy } from '#type/LocalizationResourceProxy';
+
+export { TemplateArguments } from '#type/TemplateArguments';
+
+export { CommandInfoProvider } from '#type/CommandInfoProvider';
 export { commandInfoProvider } from '#root/commandInfoProvider';
