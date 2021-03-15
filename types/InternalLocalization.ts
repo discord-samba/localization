ad6113e4839e65a78b35813b28ee7d4cdfd70baa
zajrik <zajrik@users.noreply.github.com>
2021-03-15 19:50:53 +0000

    Push staging from: 5de4f1e36720438fb07e2dbe78c03c5203ecb308

---
+++ types/InternalLocalization.ts
@@ -0,0 +1,18 @@
+import { LocalizationResrouceMetaData } from './LocalizationResourceMetaData';
+import { ResourcePath } from './ResourcePath';
+import { TemplateArguments } from './TemplateArguments';
+
+/**
+ * Represents the internal implementation of Localization that allows passing _meta
+ * for resource lookups
+ * @internal
+ */
+export interface InternalLocalization
+{
+	resource(
+		path: ResourcePath,
+		key: string,
+		args: TemplateArguments,
+		_meta: LocalizationResrouceMetaData
+	): string;
+}
